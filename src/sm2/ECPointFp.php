85a8e6b999d848936dd841e433a9b797be9e74b4
hbh112233abc <hbh112233abc@163.com>
2021-03-24 09:16:20 +0800

    修复计算mod问题

--- src/sm2/ECPointFp.php
+++ src/sm2/ECPointFp.php
@@ -131,21 +131,28 @@ class ECPointFp
         }
 
         // u = y2 * z1 - y1 * z2
-        $u = gmp_sub(
-            gmp_mul(gmp_init($other->y), $this->z),
-            gmp_mod(
-                gmp_mul($this->y->toBigInteger(), $other->z),
-                $this->curve->q
-            )
+        $y2z1 = gmp_mul($other->y->toBigInteger(), $this->z);
+        $y1z2 = gmp_mul($this->y->toBigInteger(), $other->z);
+        $u    = gmp_mod(
+            gmp_sub(
+                $y2z1,
+                $y1z2,
+            ),
+            $this->curve->q
         );
         if (gmp_cmp($u, $this->zero) !== 0) {
             return false;
         }
 
         // v = x2 * z1 - x1 * z2
-        $v = gmp_sub(
-            gmp_mul(gmp_init($other->x), $this->z),
-            gmp_mod(gmp_mul($this->x->toBigInteger(), $other->z), $this->curve->q)
+        $x2z1 = gmp_mul($other->x->toBigInteger(), $this->z);
+        $x1z2 = gmp_mul($this->x->toBigInteger(), $other->z);
+        $v    = gmp_mod(
+            gmp_sub(
+                $x2z1,
+                $x1z2
+            ),
+            $this->curve->q
         );
 
         return (gmp_cmp($v, $this->zero) !== 0);
@@ -225,47 +232,40 @@ class ECPointFp
         $x1 = $this->x->toBigInteger();
         $y1 = $this->y->toBigInteger();
         $z1 = $this->z;
-        $x2 = gmp_init($b->x);
-        $y2 = gmp_init($b->y);
+        $x2 = $b->x->toBigInteger();
+        $y2 = $b->y->toBigInteger();
         $z2 = $b->z;
         $q  = $this->curve->q;
 
+        // λ1 = x1 * z2
         $w1 = gmp_mod(
-            gmp_mul(
-                $x1,
-                $z2
-            ),
+            gmp_mul($x1, $z2),
             $q
         );
+
+        // λ2 = x2 * z1
         $w2 = gmp_mod(
-            gmp_mul(
-                $x2,
-                $z1
-            ),
+            gmp_mul($x2, $z1),
             $q
         );
-        $w3 = gmp_sub(
-            $w1,
-            $w2
-        );
+
+        // λ3 = λ1 − λ2
+        $w3 = gmp_sub($w1, $w2);
+
+        // λ4 = y1 * z2
         $w4 = gmp_mod(
-            gmp_mul(
-                $y1,
-                $z2
-            ),
+            gmp_mul($y1, $z2),
             $q
         );
+
+        // λ5 = y2 * z1
         $w5 = gmp_mod(
-            gmp_mul(
-                $y2,
-                $z1
-            ),
+            gmp_mul($y2, $z1),
             $q
         );
-        $w6 = gmp_sub(
-            $w4,
-            $w5
-        );
+
+        // λ6 = λ4 − λ5
+        $w6 = gmp_sub($w4, $w5);
 
         if (gmp_cmp($this->zero, $w3) === 0) {
             if (gmp_cmp($this->zero, $w6) === 0) {
@@ -274,80 +274,63 @@ class ECPointFp
             return $this->curve->infinity; // this == -b，则返回无穷远点
         }
 
-        $w7 = gmp_add(
-            $w1,
-            $w2
-        );
+        // λ7 = λ1 + λ2
+        $w7 = gmp_add($w1, $w2);
+
+        // λ8 = z1 * z2
         $w8 = gmp_mod(
-            gmp_mul(
-                $z1,
-                $z2
-            ),
+            gmp_mul($z1, $z2),
             $q
         );
+
+        // λ9 = λ3^2
         $w9 = gmp_mod(
-            gmp_pow(
-                $w3,
-                2
-            ),
+            gmp_pow($w3, 2),
             $q
         );
+
+        // λ10 = λ3 * λ9
         $w10 = gmp_mod(
-            gmp_mul(
-                $w3,
-                $w9
+            gmp_mul($w3, $w9),
+            $q
+        );
+
+        // λ11 = λ8 * λ6^2 − λ7 * λ9
+        $w11 = gmp_mod(
+            gmp_sub(
+                gmp_mul(
+                    $w8,
+                    gmp_pow($w6, 2)
+                ),
+                gmp_mul($w7, $w9)
             ),
             $q
         );
-        $w11 = gmp_sub(
-            gmp_mul(
-                $w8,
-                gmp_pow(
-                    $w6,
-                    2
-                )
-            ),
-            gmp_mod(
-                gmp_mul(
-                    $w7,
-                    $w9
-                ),
-                $q
-            )
-        );
 
+        // x3 = λ3 * λ11
         $x3 = gmp_mod(
-            gmp_mul(
-                $w3,
-                $w11
+            gmp_mul($w3, $w11),
+            $q
+        );
+
+        // y3 = λ6 * (λ9 * λ1 − λ11) − λ4 * λ10
+        $y3 = gmp_mod(
+            gmp_sub(
+                gmp_mul(
+                    $w6,
+                    gmp_sub(
+                        gmp_mul($w9, $w1),
+                        $w11
+                    )
+                ),
+                gmp_mul($w4, $w10)
             ),
             $q
         );
-        $y3 = gmp_sub(
-            gmp_mul(
-                $w6,
-                gmp_sub(
-                    gmp_mul(
-                        $w9,
-                        $w1
-                    ),
-                    $w11
-                )
-            ),
-            gmp_mod(
-                gmp_mul(
-                    $w4,
-                    $w10
-                ),
-                $q
-            )
-        );
 
+        // z3 = λ10 * λ8
         $z3 = gmp_mod(
-            gmp_mul(
-                $w10,
-                $w8
-            ),
+            gmp_mul($w10, $w8),
             $q
         );
 
@@ -391,25 +374,21 @@ class ECPointFp
         $q  = $this->curve->q;
         $a  = $this->curve->a->toBigInteger();
 
-        $w1 = gmp_add(
-            gmp_mul(
-                gmp_pow(
-                    $x1,
-                    2
+        // λ1 = 3 * x1^2 + a * z1^2
+        $w1 = gmp_mod(
+            gmp_add(
+                gmp_mul(
+                    gmp_pow($x1, 2),
+                    $this->three
                 ),
-                $this->three
-            ),
-            gmp_mod(
                 gmp_mul(
                     $a,
-                    gmp_pow(
-                        $z1,
-                        2
-                    )
-                ),
-                $q
-            )
+                    gmp_pow($z1, 2)
+                )
+            ),
+            $q
         );
+        // λ2 = 2 * y1 * z1
         $w2 = gmp_mod(
             gmp_mul(
                 gmp_strval($y1) << 1,
@@ -417,69 +396,58 @@ class ECPointFp
             ),
             $q
         );
+        // λ3 = y1^2
         $w3 = gmp_mod(
-            gmp_pow(
-                $y1,
-                2
-            ),
+            gmp_pow($y1, 2),
             $q
         );
+        // λ4 = λ3 * x1 * z1
         $w4 = gmp_mod(
             gmp_mul(
-                gmp_mul(
-                    $w3,
-                    $x1
-                ),
+                gmp_mul($w3, $x1),
                 $z1
             ),
             $q
         );
+        // λ5 = λ2^2
         $w5 = gmp_mod(
-            gmp_pow(
-                $w2,
-                2
-            ),
+            gmp_pow($w2, 2),
             $q
         );
-        $w6 = gmp_sub(
-            gmp_pow(
-                $w1,
-                2
+        // λ6 = λ1^2 − 8 * λ4
+        $w6 = gmp_mod(
+            gmp_sub(
+                gmp_pow($w1, 2),
+                gmp_strval($w4) << 3
             ),
-            gmp_mod(
-                gmp_strval($w4) << 3,
-                $q
-            )
+            $q
         );
 
+        // x3 = λ2 * λ6
         $x3 = gmp_mod(
-            gmp_mul(
-                $w2,
-                $w6
-            ),
+            gmp_mul($w2, $w6),
             $q
         );
-        $y3 = gmp_sub(
-            gmp_mul(
-                $w1,
-                gmp_sub(
-                    gmp_strval($w4) << 2,
-                    $w6
-                )
-            ),
-            gmp_mod(
+        // y3 = λ1 * (4 * λ4 − λ6) − 2 * λ5 * λ3
+        $y3 = gmp_mod(
+            gmp_sub(
+                gmp_mul(
+                    $w1,
+                    gmp_sub(
+                        gmp_strval($w4) << 2,
+                        $w6
+                    )
+                ),
                 gmp_mul(
                     gmp_strval($w5) << 1,
                     $w3
-                ),
-                $q
-            )
-        );
-        $z3 = gmp_mod(
-            gmp_mul(
-                $w2,
-                $w5
+                )
             ),
+            $q
+        );
+        // z3 = λ2 * λ5
+        $z3 = gmp_mod(
+            gmp_mul($w2, $w5),
             $q
         );
 
@@ -515,22 +483,13 @@ class ECPointFp
         $neg         = $this->negate();
         $Q           = $this;
         $k3bitLength = strlen(
-            gmp_strval(
-                $k3,
-                2
-            )
+            gmp_strval($k3, 2)
         );
         for ($i = $k3bitLength - 2; $i > 0; $i--) {
             $Q = $Q->twice();
 
-            $k3Bit = gmp_testbit(
-                $k3,
-                $i
-            );
-            $kBit = gmp_testbit(
-                $k,
-                $i
-            );
+            $k3Bit = gmp_testbit($k3, $i);
+            $kBit  = gmp_testbit($k, $i);
 
             if ($k3Bit !== $kBit) {
                 $Q = $Q->add($k3Bit ? $this : $neg);
